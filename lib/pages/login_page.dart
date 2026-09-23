4d8abb23a1a3d35b7e64c63f2a23637df844ddd7
TEW_Thammarat <84270286+tew080@users.noreply.github.com>
2026-09-23 22:53:49 +0700

    login_page: fix an issue where the incorrect email was displayed on the OTP verification page.

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -423,7 +423,7 @@ class _LoginPageState extends State<LoginPage> {
                     border: Border.all(color: Colors.green.shade200),
                   ),
                   child: Text(
-                    'ส่งรหัส OTP 6 หลักไปที่เมล\n$_targetEmail\nตรวจสอบในกล่องจดหมายของคุณ',
+                    'ส่งรหัส OTP 6 หลักไปที่เมล\n${!editEmail ?  selectedEmail : newSelectedEmail}\nตรวจสอบในกล่องจดหมายของคุณ',
                     textAlign: TextAlign.center,
                     style: const TextStyle(
                       color: Colors.green,
